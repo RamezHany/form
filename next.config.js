9d277b06bb673c820e8d16da54285bfb80c59a69
RamezHany <rramezhany@gmail.com>
2025-03-13 19:59:22 +0200

    Add enable/disable buttons for companies and fix build issues

--- next.config.js
+++ next.config.js
@@ -16,16 +16,10 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     unoptimized: true, // Allow images from data URLs
   },
-  // Disable some features that might cause the EPERM error
-  experimental: {
-    forceSwcTransforms: true,
-  },
   // Disable source maps in production
   productionBrowserSourceMaps: false,
   // Disable React strict mode
   reactStrictMode: false,
-  // Disable SWC minify
-  swcMinify: false,
 };
 
 module.exports = nextConfig; 
